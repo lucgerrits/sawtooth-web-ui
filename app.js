bd834c4c5cc2b5c86de1a3af95a6b0b8e253e09a
Luc Gerrits <gerrits.luc.official@gmail.com>
2019-04-13 13:26:19 +0200

    add stuff

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var config = require('./config.json');
 
 console.log("Starting sawatooth-web-ui");
 
 var indexRouter = require('./routes/index');
 
 var app = express();
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.ejs_params = {};
   res.ejs_params.title = "Sawtooth Web UI";
+  res.ejs_params.config = config;
   return next();
 });
 // view engine setup
@@ -27,12 +29,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,49 @@
 var express = require('express');
 var router = express.Router();
 var async = require('async');
-var request = require('request');
 var config = require('../config.json');
+var db = require('../lib/database');
 
 router.get('/', function (req, res, next) {
+  res.ejs_params.data = {}
   async.series([
     function (callback) {
-      var options = {
-        url: config.sawtooth.apiurl + "/blocks",
-        method: "GET",
-        json: {}
-      }
-      request(options, function (err, response, body) {
-        if (err) return next(new Error("Unable to reach sawtooth REST API (" + config.sawtooth.apiurl + ")."));
-        res.ejs_params.data = body;
-        return callback(err);
-      });
+      db.getBlocks(function (err, data) {
+        res.ejs_params.blocks = data;
+        callback(err);
+      })
+    },
+    function (callback) {
+      db.getBatches(function (err, data) {
+        res.ejs_params.batches = data;
+        callback(err);
+      })
+    },
+    function (callback) {
+      db.getTransactions(function (err, data) {
+        res.ejs_params.transactions = data;
+        callback(err);
+      })
     }
   ], function (err) {
-    if (err) return next(err);
-    res.ejs_params.title = "Current block list";
+    if (err) console.log(err);
+    res.ejs_params.title = "Dashboard";
     return res.render('index', res.ejs_params);
   })
 });
 
-
-router.get('/configuration', function (req, res, next) {
-  res.ejs_params.config = config;
-  res.ejs_params.title = "Configuration";
-  return res.render('index', res.ejs_params);
+router.post('/update-db', function (req, res, next) {
+  db.updateDb(function (err) {
+    if (err) return res.status(500).send(err.message);
+    return res.json({});
+  })
 });
+
+router.post('/force-update-db', function (req, res, next) {
+  db.forceUpdateDb(function (err) {
+    if (err) return res.status(500).send(err.message);
+    return res.json({});
+  })
+});
+
 module.exports = router;
